b413ab5a409399f5597e5a30e2f70271262de0a3
Pawel <pepawel@users.noreply.github.com>
2023-08-19 22:21:35 +0200

    Initial commit

---
+++ lib/zeitgeist.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "zeitgeist/version"
+
+module Zeitgeist
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/zeitgeist/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Zeitgeist
+  VERSION = "0.1.0"
+end
