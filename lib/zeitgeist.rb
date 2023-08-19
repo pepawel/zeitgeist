abe8f0af73583efe74c510094ae3a913c2bc3b42
Pawel <pepawel@users.noreply.github.com>
2023-08-19 22:27:06 +0200

    Fix code loading

--- lib/zeitgeist.rb
+++ lib/zeitgeist.rb
@@ -1,12 +1,20 @@
-# frozen_string_literal: true
+require 'zeitgeist/version'
 
-require_relative "zeitgeist/version"
+begin
+  require 'basic_loader'
+rescue LoadError => e
+  # We are not interested in LoadError exceptions caused by
+  # failed requires residing INSIDE basic_loader.rb file.
+  raise if e.path != 'basic_loader'
 
-require "zeitwerk"
-loader = Zeitwerk::Loader.for_gem
-loader.setup
+  require 'zeitwerk'
+  loader = Zeitwerk::Loader.for_gem
+  loader.setup
+end
 
 module Zeitgeist
   class Error < StandardError; end
   # Your code goes here...
 end
+
+Zeitgeist.const_set(:CODE_LOADER, loader) # we will need this later
